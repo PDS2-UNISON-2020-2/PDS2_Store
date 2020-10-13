2386558850a45344cebbf7438005f55e6b0191b8
mlopezrnet <mlopezramonet@gmail.com>
2020-10-13 16:06:16 -0700

    Commit inicial

---
+++ PDS2_Store/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PDS2_Store.MvcApplication" Language="C#" %>
---
+++ PDS2_Store/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PDS2_Store.Startup))]
+namespace PDS2_Store
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
