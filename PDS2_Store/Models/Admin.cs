9a49b038c15f18c65183d232e907a6c8551c2749
selene perez <selene.psesma@gmail.com>
2020-11-10 14:36:08 -0700

    update parece que algo no se subio

---
+++ PDS2_Store/Models/Admin.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PDS2_Store.Models
+{
+    public class Admin : IdentityUser
+    {
+    }
+}
