a8f8fd92adadf1ecc020758c6f49aec4ceba92bf
arodymg <arodymg97@gmail.com>
2020-11-12 19:36:40 -0800

    Le instale el dapper, y meti parte en los modelos para la solicitud de vendedor

--- PDS2_Store/Models/Admin.cs
+++ PDS2_Store/Models/Admin.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,5 +9,13 @@ namespace PDS2_Store.Models
 {
     public class Admin : IdentityUser
     {
+    }
+
+    public class RequestStatus
+    {
+        public int RequestStatusId { get; set; }
+        public string Estado { get; set; }
+        public int RequestId { get; set; }
+        public virtual Request Request { get; set; }
     }
 }
