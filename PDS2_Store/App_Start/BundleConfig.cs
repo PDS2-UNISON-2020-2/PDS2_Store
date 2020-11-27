7539e6a3a2f5ae5d535507ab4bcb314707e1a41c
mlopezrnet <mlopezramonet@gmail.com>
2020-11-27 16:06:10 -0700

    Agregar validación básica de CC -Agregar localización de validate de jQuery para que no salgan los mensajes en inglés -Mejorada un poco la apariencia -Agregar link para regresar de la pagina de Tarjetas al Index

--- PDS2_Store/App_Start/BundleConfig.cs
+++ PDS2_Store/App_Start/BundleConfig.cs
@@ -12,7 +12,8 @@ namespace PDS2_Store
 						"~/Scripts/jquery-{version}.js"));
 
 			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-						"~/Scripts/jquery.validate*"));
+						"~/Scripts/jquery.validate*",
+						"~/Scripts/messages_es.js"));
 
 			// Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
 			// para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
