2d24061df5c4c31e8571083b055494b66ce4a036
arodymg <arodymg97@gmail.com>
2020-12-02 20:42:51 -0700

    Cambio rapido, nomas cambie los estados por un enum para que no existan errores de como se escriban estados, porque si no lo hacia en los reportes iba haber problemas

--- PDS2_Store/Models/Direcciones.cs
+++ PDS2_Store/Models/Direcciones.cs
@@ -11,7 +11,7 @@ namespace PDS2_Store.Models
         public int id { get; set; }
         public string direccion { get; set; }
         public int CodigoPostal { get; set; }
-        public string Estado { get; set; }
+        public States Estado{ get; set; }
         public string Ciudad { get; set; }
     }
 
@@ -28,9 +28,44 @@ namespace PDS2_Store.Models
         public int id { get; set; }
         public string direccion { get; set; }
         public int CodigoPostal { get; set; }
-        public string Estado { get; set; }
+        public States Estado { get; set; }
         public string Ciudad { get; set; }
         [StringLength(128)]
         public string UserId { get; set; }
+    }
+
+    public enum States
+    {
+        Aguascalientes,
+        BajaCalifornia,
+        BajaCaliforniaSur,
+        Campeche,
+        Chiapas,
+        Chihuahua,
+        Coahuila,
+        Colima,
+        Durango,
+        EstadoDeMéxico,
+        Guanajuato,
+        Guerrero,
+        Hidalgo,
+        Jalisco,
+        Michoacán,
+        Morelos,
+        Nayarit,
+        NuevoLeón,
+        Oaxaca,
+        Puebla,
+        Querétaro,
+        QuintanaRoo,
+        SanLuisPotosí,
+        Sinaloa,
+        Sonora,
+        Tabasco,
+        Tamaulipas,
+        Tlaxcala,
+        Veracruz,
+        Yucatán,
+        Zacatecas
     }
 }
